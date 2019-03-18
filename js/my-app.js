75fc5a75c648c65b1db04da22bf8c6b113eb7e8e
creyess <43330888+creyess@users.noreply.github.com>
2019-03-17 21:43:48 -0600

    Add files via upload

--- js/my-app.js
+++ js/my-app.js
@@ -42,7 +42,111 @@ var myApp = new Framework7({
     	url: 'pagina-principal.html',
     	name: 'pagina-principal',
   		},
+        {
+		path: '/basicos/',
+    	url: 'basicos.html',
+    	name: 'basicos',
+  		},
+        {
+		path: '/saludables/',
+    	url: 'saludables.html',
+    	name: 'saludables',
+  		},
+
+                {
+		path: '/cuidado/',
+    	url: 'cuidado.html',
+    	name: 'cuidado',
+  		},
+       
+         {
+		path: '/carnes/',
+    	url: 'carnes.html',
+    	name: 'carnes',
+  		},
+        {
+		path: '/embutidos/',
+    	url: 'embutidos.html',
+    	name: 'embutidos',
+  		},
+    
+        {
+		path: '/jugos/',
+    	url: 'jugos.html',
+    	name: 'jugos',
+  		},
+
+        {
+		path: '/sodas/',
+    	url: 'sodas.html',
+    	name: 'sodas',
+  		},
+
+         {
+		path: '/panaderia/',
+    	url: 'panaderia.html',
+    	name: 'panaderia',
+  		},
+
+         {
+		path: '/lechesula/',
+    	url: 'lechesula.html',
+    	name: 'lechesula',
+  		},
+
+        {
+		path: '/pastas/',
+    	url: 'pastas.html',
+    	name: 'pastas',
+  		},
+
+        {
+		path: '/shampoo/',
+    	url: 'shampoo.html',
+    	name: 'shampoo',
+  		},
+
+        {
+		path: '/salchicha/',
+    	url: 'salchicha.html',
+    	name: 'salchicha',
+  		},
+
+        {
+		path: '/pina/',
+    	url: 'pina.html',
+    	name: 'pina',
+  		},
+                
+                {
+		path: '/coca-cola/',
+    	url: 'coca-cola.html',
+    	name: 'coca-cola',
+  		},
+
+        {
+		path: '/tilapia/',
+    	url: 'tilapia.html',
+    	name: 'tilapia',
+  		},
+
+        {
+		path: '/pan-integral/',
+    	url: 'pan-integral.html',
+    	name: 'pan-integral',
+  		},
+		
+         {
+		path: '/espagueti/',
+    	url: 'espagueti.html',
+    	name: 'espagueti',
+  		},
 			
+        {
+		path: '/arroz/',
+    	url: 'arroz.html',
+    	name: 'arroz',
+  		},
 	]
 });
 // Export selectors engine
